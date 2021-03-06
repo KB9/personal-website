f551cbc237de2352bb43d97e2573de4111e541b1
KB9 <kavanbickerstaff@googlemail.com>
2021-03-06 16:53:35 +0000

    Replaced dead home links with LinkedIn, GitHub, email links

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,10 @@
-import { ChakraProvider } from "@chakra-ui/react"
+import { ChakraProvider } from "@chakra-ui/react";
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+
+library.add(fab, faEnvelope);
 
 function MyApp({ Component, pageProps }) {
   return (
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
-import Link from "next/link";
-import { Box, Flex, Heading, HStack, Text } from "@chakra-ui/react"
+import { Box, Flex, Heading, Link, Stack, Text } from "@chakra-ui/react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import Layout from "../components/Layout";
 
@@ -28,18 +28,22 @@ function Home() {
               I love building stuff.
             </Text>
             <Box marginTop="30px">
-              <HStack spacing="30px">
-                <Link href="/about">
-                  <Text fontWeight="bold" cursor="pointer" textDecoration="underline">
-                    LEARN MORE ABOUT ME
-                  </Text>
+              <Stack
+                direction="row"
+                marginTop="30px"
+                spacing="16px"
+                alignItems="center"
+              >
+                <Link href="https://www.linkedin.com/in/kavan-bickerstaff-46a757133/" isExternal>
+                  <FontAwesomeIcon icon={["fab", "linkedin"]} width="32px" />
                 </Link>
-                <Link href="/contact">
-                  <Text fontWeight="bold" cursor="pointer" textDecoration="underline">
-                    GET IN TOUCH
-                  </Text>
+                <Link href="https://github.com/KB9" isExternal>
+                  <FontAwesomeIcon icon={["fab", "github"]} width="32px" />
                 </Link>
-              </HStack>
+                <Link href="mailto:kb@kavanbickerstaff.com" isExternal>
+                  <FontAwesomeIcon icon="envelope" width="32px" />
+                </Link>
+              </Stack>
             </Box>
           </Box>
           <Text
