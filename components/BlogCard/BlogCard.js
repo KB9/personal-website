1a75dad6f75c094750e9909aeaec0f7eaa990b90
KB9 <kavanbickerstaff@googlemail.com>
2021-05-19 00:17:46 +0100

    Link to Jindosh Riddle solver page from blog home page

--- components/BlogCard/BlogCard.js
+++ components/BlogCard/BlogCard.js
@@ -3,8 +3,8 @@ import { Box, Heading, LinkBox, LinkOverlay, Text } from "@chakra-ui/react";
 function BlogCard(props) {
   return (
     <LinkBox borderWidth="1px" borderRadius="lg" p="4">
-      <Heading>
-        <LinkOverlay href="#">{props.title}</LinkOverlay>
+      <Heading as="h2" size="lg">
+        <LinkOverlay href={props.link}>{props.title}</LinkOverlay>
       </Heading>
       <Text>{props.text}</Text>
       <Text>{props.timestamp}</Text>
