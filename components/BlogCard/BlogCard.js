9248d7c4793cd64a7d14699d04f4d09b37223ad3
KB9 <kavanbickerstaff@googlemail.com>
2021-06-01 02:19:32 +0100

    Client-side transitions for blog post card links

--- components/BlogCard/BlogCard.js
+++ components/BlogCard/BlogCard.js
@@ -1,10 +1,13 @@
+import Link from "next/link";
 import { Box, Heading, LinkBox, LinkOverlay, Text } from "@chakra-ui/react";
 
 function BlogCard(props) {
   return (
     <LinkBox borderWidth="1px" borderRadius="lg" p="4">
       <Heading as="h2" size="lg">
-        <LinkOverlay href={props.link}>{props.title}</LinkOverlay>
+        <Link href={props.link} passHref>
+          <LinkOverlay>{props.title}</LinkOverlay>
+        </Link>
       </Heading>
       <Text>{props.text}</Text>
       <Text>{props.timestamp}</Text>
